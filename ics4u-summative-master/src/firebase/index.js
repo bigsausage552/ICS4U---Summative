a9e39ed28bb9460acf66aff151b3b247b524c606
David Liu <david.liu4@student.tdsb.on.ca>
2024-12-10 11:30:45 -0500

    Base Code

--- ics4u-summative-master/src/firebase/index.js
+++ ics4u-summative-master/src/firebase/index.js
@@ -3,7 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyACuv7HleytcRHfxe1zS6FMfp3BHuH3DkI",
+    authDomain: "summative-c83ff.firebaseapp.com",
+    projectId: "summative-c83ff",
+    storageBucket: "summative-c83ff.firebasestorage.app",
+    messagingSenderId: "513796238711",
+    appId: "1:513796238711:web:38ff9c64519edaa3a78572"
+  };
+  
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
